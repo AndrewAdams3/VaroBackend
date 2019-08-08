582e6fd280bf147a96f728a75ce6ade459f18603
Andrew <andydrew313@gmail.com>
2019-08-08 16:45:47 -0700

    cors**

--- app.js
+++ app.js
@@ -59,21 +59,6 @@ app.use(cors());
 app.use(compression());
 
 //Cors
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  //intercepts OPTIONS method
-  if ('OPTIONS' === req.method) {
-    //respond with 200
-    res.send(200);
-  }
-  else {
-    //move on
-    next();
-  }
-});
 app.options("/*", function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
