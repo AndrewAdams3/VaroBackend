5c58417aa0c2dd4c36b8a61b330fc093978bfc5a
Andrew <andydrew313@gmail.com>
2019-08-08 16:45:11 -0700

    cors

--- app.js
+++ app.js
@@ -58,8 +58,29 @@ app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(cors());
 app.use(compression());
 
+//Cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+    //move on
+    next();
+  }
+});
+app.options("/*", function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
 //Routes
-app.options('*', cors()); // first
 app.use('/', indexRouter);
 app.use('/data/users', usersRouter);
 app.use('/data/drivebys', drivebyRouter);
