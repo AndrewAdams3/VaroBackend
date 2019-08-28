8703e795b6b1f0492d35acbc6a1286c4ec095205
Andrew Adams <andydrew313@gmail.com>
2019-08-28 16:47:35 -0700

    remove console log

--- routes/assignments.js
+++ routes/assignments.js
@@ -112,7 +112,6 @@ router.get('/target/byId/:userId', (req, res) => {
   }, (err, res2) => {
     if(err) res.send({})
     else {
-      console.log("test", res2.target);
       res.send(res2.target);
     }
   })
