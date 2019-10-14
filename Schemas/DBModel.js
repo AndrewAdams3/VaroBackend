6ee69c4d7915cdecb9b24900a892b7c655a2263e
andrewadams32 <andrewadams3@mail.fresnostate.edu>
2019-10-14 11:55:13 -0700

    remove logs on server

--- Schemas/DBModel.js
+++ Schemas/DBModel.js
@@ -59,21 +59,16 @@ var DriveBy = new Schema({
 var DriveByModel = mongoose.model('DriveBy', DriveBy);
 
 DriveByModel.watch().on("change", (update) => {
-  console.log("update", update.documentKey._id);
   if(update.operationType === 'update'){
-    console.log("updated")
     io.sockets.emit("update-db", update.documentKey._id);
   } else if( update.operationType === 'insert') {
-    console.log("new db", update.documentKey._id);
     io.sockets.emit("new-db", update.documentKey._id);
   } else if(update.operationType === 'delete') {
-    console.log("deleting", update.documentKey._id);
     io.sockets.emit("delete-db", update.documentKey._id);
   } else if(update.operationType === 'replace') {
-    console.log("relpaced")
     io.sockets.emit("update-db", update.documentKey._id);
   } else {
-    console.log("this update", update)
+    console.log("uncaught update", update)
   }
 })
 
